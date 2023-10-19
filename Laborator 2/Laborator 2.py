37f3f0359a52c2dd1b121ca0b80c9bf03ee96971
SergiuScorbureanu <sergiu.scorbureanu@gmail.com>
2023-10-19 17:48:23 +0300

    Laborator 2, ex 2

--- Laborator 2/Laborator 2.py
+++ Laborator 2/Laborator 2.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 from scipy.io import wavfile
 import sounddevice
 
-### EX 1
+### Exercitiul 1
+
 amplitudine = 1.5
 frecventa = 5
 faza = np.pi/4
@@ -21,4 +22,32 @@ plt.subplot(2, 1, 2)
 plt.plot(timp, semnal_cosinus)
 plt.title('Semnal cosinusoidal')
 
+plt.show()
+
+
+### Exercitiul 2
+
+amplitudine = 1
+frecventa = 5
+faze = [0, np.pi/4, np.pi/2, 3*np.pi/4]
+
+timp = np.linspace(0, 1, 500)
+
+semnale = []
+for faza in faze:
+    semnal = amplitudine * np.sin(2 * np.pi * frecventa * timp + faza)
+    semnale.append(semnal)
+
+plt.figure(figsize=(10, 6))
+for i, semnal in enumerate(semnale):
+    plt.plot(timp, semnal)
+
+n = np.random.normal(0, 1, 500)
+valori_snr = [0.1, 1, 10, 100]
+for snr in valori_snr:
+    zgomot = np.random.normal(0, 1 / snr, 500)
+    semnal_zgomot = semnal + zgomot
+    plt.plot(timp, semnal_zgomot)
+
+plt.title('Semnale sinusoidale')
 plt.show()
