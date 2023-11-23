fdf75dbe5b6f4877e52555ea1687ab2e48ec9ece
SergiuScorbureanu <sergiu.scorbureanu@gmail.com>
2023-11-23 17:03:27 +0200

    Laborator 6, Ex 1,2 si 4

--- Laborator 6/Laborator 6.py
+++ Laborator 6/Laborator 6.py
@@ -1,37 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fft import fft, ifft
+import pandas as pd
 
 ### Exercitiul 1
 # Generare vector aleator x
 N = 100
 x = np.random.rand(N)
 
-# # Afișare semnal inițial
-# plt.subplot(221)
-# plt.plot(x)
-# plt.title('Semnal Initial')
-#
-# # Iteratia 1: x ← x * x
-# x = x * x
-# plt.subplot(222)
-# plt.plot(x)
-# plt.title('Iteratia 1: x ← x * x')
-#
-# # Iteratia 2: x ← x * x
-# x = x * x
-# plt.subplot(223)
-# plt.plot(x)
-# plt.title('Iteratia 2: x ← x * x')
-#
-# # Iteratia 3: x ← x * x
-# x = x * x
-# plt.subplot(224)
-# plt.plot(x)
-# plt.title('Iteratia 3: x ← x * x')
-#
-# plt.tight_layout()
-# plt.show()
+# Afișare semnal inițial
+plt.subplot(221)
+plt.plot(x)
+plt.title('Semnal Initial')
+
+# Iteratia 1
+x = x * x
+plt.subplot(222)
+plt.plot(x)
+plt.title('Iteratia 1')
+
+# Iteratia 2
+x = x * x
+plt.subplot(223)
+plt.plot(x)
+plt.title('Iteratia 2')
+
+# Iteratia 3
+x = x * x
+plt.subplot(224)
+plt.plot(x)
+plt.title('Iteratia 3')
+
+plt.tight_layout()
+plt.show()
 
 # Observatie: semnalul se modifica in timpul fiecarei operatii. Acesta se inmulteste cu el insusi si astfel creste exponential
 
@@ -56,12 +57,12 @@ print("Produsul direct p(x) * q(x):", rez_convolutie)
 
 # Metoda 2: Inmultirea cu FFT
 # Zero-padding pentru a evita pierderea informației în FFT
-p_padded = np.pad(coeficienti_p, (0, N), mode='constant')
-q_padded = np.pad(coeficienti_q, (0, N), mode='constant')
+p_pad = np.pad(coeficienti_p, (0, N), mode='constant')
+q_pad = np.pad(coeficienti_q, (0, N), mode='constant')
 
 # Aplicarea FFT pe p(x) și q(x)
-p_fft = fft(p_padded)
-q_fft = fft(q_padded)
+p_fft = fft(p_pad)
+q_fft = fft(q_pad)
 
 # Calcularea produsului în domeniul frecvenței
 rez_fft = p_fft * q_fft
@@ -71,3 +72,36 @@ r_fft = ifft(rez_fft).real
 
 # Afișarea rezultatului
 print("Produsul prin FFT:", r_fft)
+
+
+### Exercitiul 4
+# (a) si (b)
+
+data = pd.read_csv('TrainL6.csv')
+
+# Convertirea coloanei Datetime la tipul de date datetime
+data['Datetime'] = pd.to_datetime(data['Datetime'], format='%d-%m-%Y %H:%M')
+
+# Selectarea unei portiuni de 3 zile
+data_start = '25-08-2012 00:00'
+data_sfarsit = '27-08-2012 23:00'
+date_selectate = data[(data['Datetime'] >= data_start) & (data['Datetime'] <= data_sfarsit)]
+
+# Definirea semnalului x (nr de vehicule)
+x = date_selectate['Count'].values
+
+# Definirea dimensiunilor diferite ale ferestrei
+dimensiuni_fereastra = [5, 9, 13, 17]
+
+# Filtrarea cu media alunecatoare pentru diferite dimensiuni ale ferestrei
+for dim in dimensiuni_fereastra:
+    semnal_netezit = np.convolve(x, np.ones(dim) / dim, 'valid')
+
+    # Afisarea rezultatelor
+    plt.figure()
+    plt.plot(date_selectate['Datetime'][:-dim+1], semnal_netezit)
+    plt.title(f'Semnal netezit cu fereastra {dim}')
+    plt.xlabel('Datetime')
+    plt.ylabel('Semnal')
+    plt.show()
+
