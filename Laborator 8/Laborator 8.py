c8a263fa23b003919be501476d8dee64e1f06ada
SergiuScorbureanu <sergiu.scorbureanu@gmail.com>
2023-12-14 16:54:10 +0200

    Laborator 8, Ex 1: a,b,c

--- Laborator 8/Laborator 8.py
+++ Laborator 8/Laborator 8.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from statsmodels.tsa.ar_model import AutoReg
 
 ### Exercitiul 1
 ### A
@@ -16,7 +18,7 @@ trend = 0.0005 * t**2 + 0.005 * t + 10
 # Definim componenta sezonala prin suma a 2 frecvente
 sezon = 5 * np.sin(2 * np.pi * t / 50) + 3 * np.cos(2 * np.pi * t / 30)
 
-# Generam componente de zgomot alb gaussian pentru variabilitate mica
+# Generam componente de zgomot alb gaussian
 zgomot = np.random.normal(0, 2, N)
 
 # Apelam toate componentele pentru a obține seria de timp finala
@@ -44,3 +46,40 @@ plt.legend()
 plt.tight_layout()
 plt.show()
 
+### B
+
+# Calculul autocorelației folosind np.correlate
+autocorelatie = np.correlate(seria_de_timp, seria_de_timp, mode='full')
+
+# Normalizarea autocorelației la intervalul [-1, 1]
+autocorelatie /= np.max(np.abs(autocorelatie))
+
+# Desenarea vectorului de autocorelație
+plt.figure(figsize=(12, 4))
+plt.plot(np.arange(-N+1, N), autocorelatie, label='Autocorelatie')
+plt.title('Vector autocorelatie')
+plt.xlabel('Lag')
+plt.ylabel('Autocorelatie')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+### C
+
+# Dimensiunea modelului AR
+p = 7
+
+# Crearea si antrenarea modelului AR
+model_ar = AutoReg(seria_de_timp, lags=p)
+model_ar_fit = model_ar.fit()
+
+# Obtinerea predictiilor
+predictions = model_ar_fit.predict(start=p, end=N - 1, dynamic=False)
+
+# Desenam seria de timp originala si predicțiile
+plt.figure(figsize=(14, 6))
+plt.plot(seria_de_timp, label='Serie de Timp Originala', color='blue')
+plt.plot(range(p, N), predictions, label='Predicții AR', color='red', linestyle='dashed')
+plt.title('Serie de Timp si Predictiile Modelului AR')
+plt.legend()
+plt.show()
